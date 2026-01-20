0c0185c8c2f8b3dc2926e9ec7c9e4df1c02b92ea
txasw <txasw@outlook.co.th>
2026-01-21 05:53:07 +0700

    dd

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -2,7 +2,11 @@ import { filterUserById } from "../p03"
 
 export async function safeFetchUser(id: number) {
     if (id <= 0) return null
-    const user = await filterUserById(id)
-    if (!user) return null
-    return user
+    try {
+        const user = await filterUserById(id)
+        if (!user) return null
+        return user
+    }catch{
+        return null
+    }
 }
