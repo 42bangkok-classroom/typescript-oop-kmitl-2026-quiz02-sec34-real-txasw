3178d337dfe9a51780d1d4c14f4340014204178a
txasw <txasw@outlook.co.th>
2026-01-21 06:14:42 +0700

    หฟฟหกฟหก

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -2,19 +2,26 @@ import axios from "axios";
 import { filterUserById } from "../p03";
 
 async function getTodos() {
-    const res = await axios.get("https://jsonplaceholder.typicode.com/todos")
-    const todos = res.data
-    return todos || []
+    try {
+        const res = await axios.get("https://jsonplaceholder.typicode.com/todos")
+        const todos = res.data
+        return todos || []
+    } catch (error) {
+        return []
+    }
 }
 
 export async function getTodosByUserId(id: number) {
-    const user = await filterUserById(id)
-    // if (!user) return "Invalid id"
-    if (typeof user === "string") return "Invalid id"
+    try {
+        const user = await filterUserById(id)
+        if (typeof user === "string") return "Invalid id"
 
-    const todos = await getTodos()
+        const todos = await getTodos()
 
-    const user_todos = todos.filter((todo: {userId:number}) => todo.userId == id)
-
-    return {...user, todos: user_todos ?? []}
+        const user_todos = todos.filter((todo: {userId:number}) => todo.userId == id)
+        
+        return { ...user, todos: user_todos ?? [] }
+    } catch (error) {
+        return "Invalid id"
+    }
 }
