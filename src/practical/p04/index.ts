14d8391cd44170f68419bced3a263c0e7c98b7e6
txasw <txasw@outlook.co.th>
2026-01-21 05:31:03 +0700

    asdsa

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,15 +1,3 @@
-import axios from "axios";
-import { getPostalAddress } from "../p01";
-import { filterUserById } from "../p03";
-
 export async function getTodosByUserId(id: number) {
-
-    const todos = (await axios.get("https://jsonplaceholder.typicode.com/todos")).data
-    const user = await filterUserById(id)
-    
-    return {
-        user: user,
-        todos: todos.filter((todo: { userId: number; }) => todo.userId === id)
-    };
-    
+        
 }
