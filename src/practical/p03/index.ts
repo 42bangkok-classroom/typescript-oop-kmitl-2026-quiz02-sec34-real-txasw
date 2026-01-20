bf5cec6265441ac1b43cee5ed197fa3d29a67b91
txasw <txasw@outlook.co.th>
2026-01-21 05:20:57 +0700

    tb3

--- src/practical/p03/index.ts
+++ src/practical/p03/index.ts
@@ -2,9 +2,7 @@ import { getPostalAddress } from "../p01";
 
 export async function filterUserById(id: number) {
     const users = await getPostalAddress()
-    const user = users.find(user => user.id == id)
-    if (!user) {
-        return "Invalid id"
-    }
-    return user;
+    const found = users.filter((user: {id:number}) => user.id == id)
+    if (!found) return "Invalid id"
+    return found
 }
