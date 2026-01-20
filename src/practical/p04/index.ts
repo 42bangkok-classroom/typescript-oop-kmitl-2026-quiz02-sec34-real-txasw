ffc19e673d50aa2797b497c1d93baa6e1d18653b
txasw <txasw@outlook.co.th>
2026-01-21 05:48:02 +0700

    asdad

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,3 +1,19 @@
+import axios from "axios";
+import { filterUserById } from "../p03";
+
+async function getTodos() {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/todos")
+    const todos = res.data
+    return todos ?? []
+}
+
 export async function getTodosByUserId(id: number) {
-        
+    const user = await filterUserById(id)
+    if (!user) return "Invalid id"
+
+    const todos = await getTodos()
+
+    const user_todos = todos.filter((todo: {userId:number}) => todo.userId == user.id)
+
+    return {...user, todos: user_todos??[]}
 }
