4cbdf74e209ff81ef1f9339494cba30219e9cf86
txasw <61960662+txasw@users.noreply.github.com>
2026-01-21 04:52:41 +0700

    test blinding

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,49 +1,16 @@
 import axios from "axios"
 
-type User = {
-  id: number
-  name: string
-  username: string
-  email: string
-  address: {
-    street: string
-    suite: string
-    city: string
-    zipcode: string
-    geo: {
-      lat: string
-      lng: string
-    }
-  } | null
-  phone: string
-  website: string
-  company: {
-    name: string
-    catchPhrase: string
-    bs: string
-  }
-}
-
-
-export async function getUsers() {
-    const users: User[] = (await axios.get("https://jsonplaceholder.typicode.com/users")).data
-
-    const usersArray = Array.from(users)
-
-    if (!usersArray) return []
-    return usersArray
-}
-
 export async function getPostalAddress() {
-    
-    const usersArray = await getUsers()
+  const res = await axios.get("https://jsonplaceholder.typicode.com/users")
+  const users = res.data
 
-    const usersMap = usersArray.map(user => ({
-        id: user.id,
-        name:user.name,
-        phone:user.phone,
-        address: user.address ?? null
-    }))
-    
-    return usersMap
+  const output = users.map(user => ({
+    id: user.id,
+    name: user.name,
+    phone: user.phone,
+    address: user.address
+  }))
+  return output
 }
+
+getPostalAddress()
